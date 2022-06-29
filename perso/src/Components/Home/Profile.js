533b7a48c595719fd03c92809d516e1da388422c
christ <christikonga83@gmail.com>
2022-06-29 23:12:14 +0200

    Structure of project

---
+++ perso/src/Components/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div className='prfile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='cols'>
+                    <a href='#'>
+                        <i className='fa fa-facebook-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-google-plus-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-linkedin'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
