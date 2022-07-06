c4ca9da584eba253ea5ae2d436e9e9d1ece3f5ed
christ <christikonga83@gmail.com>
2022-07-06 16:38:40 +0200

    Navbar

--- perso/src/Components/Header/Header.js
+++ perso/src/Components/Header/Header.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
+import ScrollService from './ScrollService/ScrollService';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesoneIcon} from '@fortawesome/react-fontawesome';
+import '../styles/Header.css';
+import Home from '../Home/Home';
 
 export default function Header() {
+  const [selectedScreen,setSelectedScreen] = useState(0)
+  const [showHeaderOptions,setShowHeaderOptions] = useState(false)
+
+  const updateCurrentScreen = (currentScreen)=>{
+    if(currentScreen || !currentScreen.screenInView)
+    return;
+    let screenIndex = Home(currentScreen.screenInView);
+    if(screenIndex<0)
+    return;
+  }
+  let currentScreenSubscription =ScrollService.currenScreenBroadCaster.subscribe(updateCurrentScreen);
   return (
     <div className='header-container'>
 
